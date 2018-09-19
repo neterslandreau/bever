a8089273f5b5ccc832d67cab59e9419e13105d0f
Niles Rowland <neterslandreau@gmail.com>
2018-09-19 14:03:29 -0400

    add title to main logo.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12">
-				<img class="img-responsive" alt="Main Image" src="{{ config('app.site-logo', 'https://via.placeholder.com/450x250/000000') }}">
+				<img class="img-responsive" alt="Main Image" src="{{ config('app.site-logo', 'https://via.placeholder.com/450x250/000000') }}" title=" {{ config('app.company-name', 'Company Name') }}">
 				<div class="intro-text">
 					<span class="name">{{ config('app.company-name', 'Company Name') }}</span>
 					<hr class="star-light">
